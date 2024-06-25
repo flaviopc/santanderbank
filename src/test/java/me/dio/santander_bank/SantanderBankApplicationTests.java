434e157a91c35d89ff17f340d9cd8fe3a0b5279e
Flávio Paixão <flavio_pc@outlook.com>
2024-06-25 13:43:06 -0300

    criação do projeto

---
+++ src/test/java/me/dio/santander_bank/SantanderBankApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dio.santander_bank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SantanderBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
