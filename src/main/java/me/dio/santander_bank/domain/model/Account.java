e752c4f9426376d58c02fecf4ee9422d49594d11
Flávio Paixão <flavio_pc@outlook.com>
2024-06-25 14:18:35 -0300

    fix(Account): Defini a conta como uma entidade

--- src/main/java/me/dio/santander_bank/domain/model/Account.java
+++ src/main/java/me/dio/santander_bank/domain/model/Account.java
@@ -3,10 +3,12 @@ package me.dio.santander_bank.domain.model;
 import java.math.BigDecimal;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
+@Entity
 public class Account {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
