78c88db05af0592c69cd84ac90cda01bfa92abd1
Flávio Paixão <flavio_pc@outlook.com>
2024-06-26 13:00:12 -0300

    feat(UserController): criação do UserController e exposição dos endpoints

--- src/main/java/me/dio/santander_bank/domain/model/Card.java
+++ src/main/java/me/dio/santander_bank/domain/model/Card.java
@@ -17,4 +17,29 @@ public class Card {
     private String number;
     @Column(name = "available_limit", precision = 13, scale = 2)
     private BigDecimal limit;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public BigDecimal getLimit() {
+        return limit;
+    }
+
+    public void setLimit(BigDecimal limit) {
+        this.limit = limit;
+    }
+
 }
